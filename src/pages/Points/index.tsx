de9411fa010fe99235a6287361369c4b4746bfd3
Rafael lima <rafaellrf09@gmail.com>
2020-06-05 12:51:26 -0300

    alterando imagen recebida

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -17,7 +17,7 @@ interface Item {
 interface Point {
   id: number;
   name: string;
-  image: string;
+  image_url: string;
   latitude: number;
   longitude: number;
 }
@@ -130,7 +130,7 @@ const Points = () => {
                                 coordinate={{ latitude: point.latitude, longitude: point.longitude}}
                               >
                                   <View style={styles.mapMarkerContainer}>
-                                      <Image style={styles.mapMarkerImage} source={{ uri: point.image}} />
+                                      <Image style={styles.mapMarkerImage} source={{ uri: point.image_url}} />
                                       <Text style={styles.mapMarkerTitle}>{point.name}</Text>
                                   </View>
                               </Marker>
